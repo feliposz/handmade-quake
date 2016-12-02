4a2c7436dc4ae15f522c5a2f622bcf10cb22757d
feliposz <feliposz@gmail.com>
2016-12-02 01:40:07 -0200

    4.2 - Wrapping the File Functions

--- sys.h
+++ sys.h
@@ -2,3 +2,9 @@
 
 void Sys_Shutdown(void);
 void Sys_SendKeyEvents(void);
+int Sys_FileOpenRead(char *, int *);
+int Sys_FileOpenWrite(char *);
+void Sys_FileClose(int);
+void Sys_FileSeek(int, int);
+int Sys_FileRead(int, void *, int);
+int Sys_FileWrite(int, void *, int);
--- sys_win.c
+++ sys_win.c
@@ -37,6 +37,128 @@ float Sys_FloatTime(void)
     return (float) GTimePassed;
 }
 
+// File functions
+
+#define MAX_HANDLES 10
+
+static FILE *FileHandles[MAX_HANDLES];
+
+static int FindHandle()
+{
+    for (int i = 0; i < MAX_HANDLES; i++)
+    {
+        if (FileHandles[i] == NULL)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+static int FileLength(int Handle)
+{
+    FILE *f = FileHandles[Handle];
+    if (f == NULL)
+    {
+        return -1;
+    }
+    int Pos = ftell(f);
+    fseek(f, 0, SEEK_END);
+    int End = ftell(f);
+    fseek(f, Pos, SEEK_SET);
+    return End;
+}
+
+int Sys_FileOpenRead(char *Path, int *Size)
+{
+    int HandleIndex = FindHandle();
+    FILE *FileHandle;
+    fopen_s(&FileHandle, Path, "rb");
+    if (!FileHandle)
+    {
+        if (Size != NULL)
+        {
+            *Size = -1;
+        }
+        return -1;
+    }
+    FileHandles[HandleIndex] = FileHandle;
+    *Size = FileLength(HandleIndex);
+    return HandleIndex;
+}
+
+int Sys_FileOpenWrite(char *Path)
+{
+    int HandleIndex = FindHandle();
+    FILE *FileHandle;
+    fopen_s(&FileHandle, Path, "wb");
+    if (!FileHandle)
+    {
+        return -1;
+    }
+    FileHandles[HandleIndex] = FileHandle;
+    return HandleIndex;
+}
+
+void Sys_FileClose(int Handle)
+{
+    if ((Handle < 0) || (Handle >= MAX_HANDLES))
+    {
+        return;
+    }
+    FILE *f = FileHandles[Handle];
+    if (!f)
+    {
+        return;
+    }
+    fclose(f);
+    FileHandles[Handle] = NULL;
+}
+
+void Sys_FileSeek(int Handle, int Position)
+{
+    if ((Handle < 0) || (Handle >= MAX_HANDLES))
+    {
+        return;
+    }
+    FILE *f = FileHandles[Handle];
+    if (!f)
+    {
+        return;
+    }
+    fseek(f, Position, SEEK_SET);
+}
+
+int Sys_FileRead(int Handle, void *Dest, int Count)
+{
+    if ((Handle < 0) || (Handle >= MAX_HANDLES))
+    {
+        return -1;
+    }
+    FILE *f = FileHandles[Handle];
+    if (!f)
+    {
+        return -1;
+    }
+    int BytesRead = (int) fread_s(Dest, Count, 1, Count, f);
+    return BytesRead;
+}
+
+int Sys_FileWrite(int Handle, void *Source, int Count)
+{
+    if ((Handle < 0) || (Handle >= MAX_HANDLES))
+    {
+        return -1;
+    }
+    FILE *f = FileHandles[Handle];
+    if (!f)
+    {
+        return -1;
+    }
+    int BytesWriten = (int)fwrite(Source, 1, Count, f);
+    return BytesWriten;
+}
+
 // Main window functions
 
 void Sys_Shutdown(void)
@@ -60,6 +182,30 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     GlobalInstance = hInstance;
 
     COM_ParseCmdLine(lpCmdLine);
+
+#if 1
+    // Test file functions
+    int Size;
+    int ReadHandle = Sys_FileOpenRead("..\\sys_win.c", &Size);
+    if (ReadHandle == -1)
+    {
+        int b = 0; b;
+    }
+    uint8_t *Buffer = malloc(Size);
+    Sys_FileRead(ReadHandle, Buffer, Size);
+
+    int WriteHandle = Sys_FileOpenWrite("..\\temp\\sys_win.out");
+    if (ReadHandle == -1)
+    {
+        int b = 0; b;
+    }
+    Sys_FileWrite(WriteHandle, Buffer, Size);
+
+    Sys_FileClose(ReadHandle);
+    Sys_FileClose(WriteHandle);
+
+    free(Buffer);
+#endif
 
     float TimeAccumulated = 0;
     float TargetTime = 1.0f / 60.0f;
